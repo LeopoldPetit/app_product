5bef36ddbb7f54df72f0f41bc03849e7600f86c8
pmp59100 <leopoldpetit59@gmail.com>
2023-10-11 19:57:45 +0200

    V1.2

--- src/App.js
+++ src/App.js
@@ -1,129 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './assets/App.css';
+import {Menu} from "./Components/ToolBar";
+import {ProductTable} from "./Components/ProductTable";
+import {useState} from "react";
+import {data_MacBook_product} from "./data/data_MacBook";
 
+const FirstProduct = {
+    name: data_MacBook_product.name,
+    level: data_MacBook_product.level,
+    category: data_MacBook_product.category,
+    code_barre: data_MacBook_product.code_barre,
+};
 
-
-const data_MacBook_product = {
-    name: "Apple MacBook Pro 17",
-    level: "rouge",
-    category: "Laptop",
-    code_barre: "2429249924",
-}
-function Product({ data }){
-
-  return(
-      <tr className="border-b bg-gray-800 border-gray-700">
-        <th scope="row" className="px-6 py-4 font-medium whitespace-nowrap text-white">
-            {data.name}
-        </th>
-        <td className="px-6 py-4">
-            {data.level}
-        </td>
-        <td className="px-6 py-4">
-            {data.category}
-        </td>
-        <td className="px-6 py-4">
-            {data.code_barre}
-        </td>
-        <td className="px-6 py-4 text-right">
-          <a href="#" className="font-medium text-blue-500 hover:underline">Edit</a>
-        </td>
-      </tr>
-  )
-}
-
-function Menu(){
-    return(
-
-        <nav className=" border-gray-200 bg-gray-900">
-            <div className="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto p-4">
-                <a href="https://flowbite.com" className="flex items-center">
-                    <img src="/logo.svg" className="h-16 mr-3" alt="Flowbite Logo" />
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">My product App</span>
-                </a>
-                <div className="flex items-center md:order-2">
-                    <a href="#" className="text-white   focus:ring-4  font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-blue-800">Login</a>
-                    <button data-collapse-toggle="mega-menu-icons" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm  rounded-lg md:hidden  focus:outline-none focus:ring-2 focus:ring-gray-200 text-gray-400 hover:bg-gray-700" aria-controls="mega-menu-icons" aria-expanded="false">
-                        <span className="sr-only">Open main menu</span>
-                        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-                        </svg>
-                    </button>
-                </div>
-                <div id="mega-menu-icons" className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
-                    <ul className="flex flex-col mt-4 font-medium md:flex-row md:space-x-8 md:mt-0">
-                        <li>
-                            <a href="#" className="block py-2 pl-3 pr-4 border-b md:border-0 md:p-0 text-blue-500 md:hover:text-blue-500 hover:bg-gray-700 hover:text-blue-500 md:hover:bg-transparent border-gray-700" aria-current="page">Home</a>
-                        </li>
-                        <li>
-                            <button id="mega-menu-icons-dropdown-button" data-dropdown-toggle="mega-menu-icons-dropdown" className="flex items-center justify-between w-full py-2 pl-3 pr-4 font-medium border-b md:w-auto md:border-0 md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-blue-500 md:hover:bg-transparent border-gray-700">
-                                Rayons
-                                <svg className="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-                                </svg>
-                            </button>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-    )
-}
-function ProductTable(){
-  return(
-      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table className="w-full text-sm text-left text-gray-400">
-          <thead className="text-xs uppercase bg-gray-700 text-gray-400">
-          <tr>
-            <th scope="col" className="px-6 py-3">
-              Product name
-            </th>
-            <th scope="col" className="px-6 py-3">
-              <div className="flex items-center">
-                Level
-                <a href="#"><svg className="w-3 h-3 ml-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                  <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
-                </svg></a>
-              </div>
-            </th>
-            <th scope="col" className="px-6 py-3">
-              <div className="flex items-center">
-                Category
-                <a href="#"><svg className="w-3 h-3 ml-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                  <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
-                </svg></a>
-              </div>
-            </th>
-            <th scope="col" className="px-6 py-3">
-              <div className="flex items-center">
-                Code Barre
-                <a href="#"><svg className="w-3 h-3 ml-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                  <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
-                </svg></a>
-              </div>
-            </th>
-            <th scope="col" className="px-6 py-3">
-              <span className="sr-only">Edit</span>
-            </th>
-          </tr>
-          </thead>
-          <tbody>
-            <Product data={data_MacBook_product}/>
-            <Product data={data_MacBook_product}/>
-            <Product data={data_MacBook_product}/>
-            <Product data={data_MacBook_product}/>
-            <Product data={data_MacBook_product}/>
-          </tbody>
-        </table>
-      </div>
-  )
-}
 function App() {
+    const [products, setProducts] = useState([FirstProduct]);
+    const addProduct = (newProduct) => {
+        setProducts([...products, newProduct]);
+    };
+
   return (
       <div className="bg-darkblue min-h-screen">
-          <Menu/>
-          <ProductTable/>
+          <Menu onAddProduct={addProduct}/>
+          <ProductTable products={products}/>
       </div>
 
   );
